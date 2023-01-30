251a4de2d7e5d60886ee343d751e6cd7300f5cf8
yanxiang <yx18221916288@163.com>
2023-01-30 22:56:27 +0800

    feat: 暂存

--- controllers/index.js
+++ controllers/index.js
@@ -8,15 +8,22 @@ class IndexController {
   constructor() {}
 
   async index(ctx, next) {
-  //   const data = await indexModel.getXiaomiDatas({
-  //     swiper: true,
-  //     phone: true,
-  //     field: true
-  //   });
+    const data = await indexModel.getXiaomiDatas({
+      swiper: true,
+      phone: true,
+      field: true
+    });
+
+    const swiperData = data.swiper_data,
+      phoneData = data.phone_data,
+      fieldData = data.field_data;
 
 
     await ctx.render('index', {
-      pageConf: pageConf.index
+      pageConf: pageConf.index,
+      swiperData,
+	    phoneData,
+	    fieldData
     })
   }
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
-import '../scss/index.scss';
-console.log('index')
+import '../scss/common.scss';
+import '../scss/iconfont.css';
+import '../scss/carousel.scss';
+import '../scss/header.scss';
+import '../scss/home_title.scss';
+import '../scss/list_board.scss';
+import '../scss/footer.scss';
+
+import HeaderNav from '../modules/HeaderNav';
+
+
+;(async ($) => {
+  const phonedata = JSON.parse($('#J_phoneData').html()),
+    fielddata = JSON.parse($('#J_fieldData').html()),
+    swiperdata = JSON.parse($('#J_swiperData').html());
+
+  const init = () => {
+    new HeaderNav(phonedata);
+  }
+
+  init();
+})(jQuery);
